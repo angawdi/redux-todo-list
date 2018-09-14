cf327264fa214d62dc28e9f79ac312f697952a73
Sky <anvandp18@gmail.com>
2018-09-14 13:37:45 -0700

    finish but still have bug

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,30 @@ import Todos from './Todos'
 import TodoForm from './TodoForm'
 
 class App extends Component {
+	constructor(){
+		super()
+		this.state = {
+			 todos: []
+		}
+	}
+
+	addTodo = (item) => {
+		console.log('adding new item', item);
+		let newItem = {
+			id: this.state.todos.length,
+			task: item,
+			completed: false
+		}
+		let todosTemp = [...this.state.todos, newItem];
+		this.setState({ todos: todosTemp })
+	}
+
   render () {
     return (
       <div className="App">
         <h1>Redux TODOS!</h1>
-        <TodoForm />
-        <Todos />
+        <TodoForm addTodo={this.addTodo} />
+        <Todos items={this.state.todos} />
       </div>
     )
   }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,30 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Todos extends Component {
-  state = {
-    todos: [
-      {
-        id: 0,
-        task: 'Test this Todo Page',
-        completed: false
-      },
-      {
-        id: 1,
-        task: 'Learn Redux',
-        completed: false
-      },
-      {
-        id: 2,
-        task: 'Learn React',
-        completed: true
-      }
-    ]
-  }
+  
   render () {
     return (
       <div>
         <ul>
-          {this.state.todos.map(todo => (
+          {this.props.todos.map(todo => (
             <li key={todo.id}>{todo.task}: {todo.completed.toString()}</li>
           ))}
         </ul>
